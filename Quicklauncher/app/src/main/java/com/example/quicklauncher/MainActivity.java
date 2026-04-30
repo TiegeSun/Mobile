3fd83de6497f4de8365effffc09bb76aeb72c2e5
Tiege Sun <2213902602@qq.com>
2026-05-01 02:50:14 +0800

    Add Quicklauncher project and core elements diary

---
+++ Quicklauncher/app/src/main/java/com/example/quicklauncher/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.quicklauncher;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        Button secondActivity = (Button) findViewById(R.id.secondActivityBtn);
+        secondActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent startIntent = new Intent(MainActivity.this, SecondActivity.class);
+                startIntent.putExtra("com.example.quicklauncher.SOMETHING", "HELLO WORLD!");
+                startActivity(startIntent);
+            }
+        });
+
+        Button googleBtn = (Button) findViewById(R.id.googleBtn);
+        googleBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String google = "https://www.google.com";
+                Uri webaddress = Uri.parse(google);
+                Intent gotoGoogle = new Intent(Intent.ACTION_VIEW, webaddress);
+                if (gotoGoogle.resolveActivity(getPackageManager()) != null) {
+                    startActivity(gotoGoogle);
+                }
+            }
+        });
+    }
+}
